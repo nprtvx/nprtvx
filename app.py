d7ec5f81af8f744f5610826cc5a06771b588537a
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-07-21 03:38:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,46 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+home = f"""
+<div class='home' id='home'>
+<h1>nprtvx</h1>
+<p>welcome</p>
+</div>
+<style>
+* {{
+margin: 0;
+padding: 0;
+box-sizing: border-box;
+}}
+.home {{
+position: absolute;
+top: 0;
+bottom: 0;
+right: 0;
+left: 0;
+box-shadow: 0 0 2rem inset;
+display: flex;
+align-items: center;
+justify-content: center;
+flex-direction: column;
+}}
+.home h1 {{
+font-size: 48px;
+color: #fffe;
+background-color: #000d;
+text-align: center;
+margin: 26px 26px 0 0;
+padding: 11px 26px 11px 26px;
+}}
+</style>
+"""
+
 @app.route('/')
 def index():
-  return f"""
+  return home
+
+popeye = f"""
     <div class='popeye' id='popeye'></div>
     <style>
       .popeye {{
@@ -17,3 +54,7 @@ def index():
       document.append(popeye*8)
     </script>
   """
+
+@app.route('/popeye'):
+def index():
+  return popeye
