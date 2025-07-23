ab0600ac96c96c4451df5eec21322157d772d55b
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-07-23 22:08:05 +0530

    Update home.py

--- src/home.py
+++ src/home.py
@@ -11,7 +11,7 @@ top: 0;
 bottom: 0;
 right: 0;
 left: 0;
-box-shadow: 0 0 0 12rem #8926 inset;
+box-shadow: 0 0 0 100vmin #8926 inset;
 display: flex;
 align-items: center;
 justify-content: center;
