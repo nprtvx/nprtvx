7391c9b1d4ed4820e5768a5186cab2348a4ac53c
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-08-23 02:54:16 +0530

    Update home.py

--- src/home.py
+++ src/home.py
@@ -10,39 +10,3 @@ home = f"""
 <script src='/static/js/nxtitle.js'></script>
 </body>
 """
-
-style = f"""
-<style>
-.nprtvx {{
-position: absolute;
-top: 0;
-bottom: 0;
-left: 0;
-right: 0;
-background-color: black;
-color: white;
-}}
-#nxtitle {{
-position: absolute;
-top: 11px;
-left: 11px;
-text-decoration: none;
-font-size: 48px;
-letter-spacing: 1em;
-line-height: 0em;
-height: 0rem;
-background: red;
-text-align: center;
-width: 62vmin;
-margin: 2rem 0;
-padding: 0;
-text-transform: uppercase;
-}}
-</style>
-"""
-
-script = f"""
-<script>
-const nxtitle = document.getElementById("nxtitle");
-</script>
-"""
