dd5adef5f06c98207dbdfa966975addd9c45a8cd
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-08-23 02:36:25 +0530

    Update home.py

--- src/home.py
+++ src/home.py
@@ -23,6 +23,17 @@ color: white;
 position: absolute;
 top: 11px;
 left: 11px;
+text-decoration: none;
+font-size: 48px;
+letter-spacing: 1em;
+line-height: 0em;
+height: 0rem;
+background: red;
+text-align: center;
+width: 62vmin;
+margin: 2rem 0;
+padding: 0;
+text-transform: uppercase;
 }}
 </style>
 """
