2a55e03364ac4146bcc5efcef7a888db95ddf447
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-08-23 01:49:23 +0530

    Update home.py

--- src/home.py
+++ src/home.py
@@ -1,12 +1,15 @@
 home = f"""
+<body>
 <div class="nprtvx">
 <h1 id="nxtitle">nprtvx</h1>
 <div class="section-nm">
 
 </div>
+</body>
 """
 
 style = f"""
+<style>
 .nprtvx {{
 position: absolute;
 top: 0;
@@ -15,12 +18,19 @@ left: 0;
 right: 0;
 background-image: linear-gradient(26deg, black, white);
 }}
+</style>
 """
 
 script = f"""
+<script>
 const nxtitle = document.getElementById("nxtitle");
 nxtitle.addEventListener("click", (event) {{
 console.log(event);
 location.href = "/";
 }});
+</script>
 """
+
+with open('templates/home.html', 'w') as page:
+  page.write(style+home+script)
+  page.close()
