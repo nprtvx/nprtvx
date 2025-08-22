2f28eff08b0cdf322736548210522610be78b6a7
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-08-23 02:55:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,14 +3,14 @@ from flask import Flask, render_template, abort
 from werkzeug.middleware.dispatcher import DispatcherMiddleware
 from a2wsgi import ASGIMiddleware
 import os
-from src.home import home, style, script
+from src.home import home
 
 def create_page(page_name: str | None = None):
   if page_name is None:
     page_name="home"
   if page_name:
     with open(f"templates/{page_name}.html", 'w') as page:
-      page.write(style+home+script)
+      page.write(home)
       page.close()
 
 create_page("home")
