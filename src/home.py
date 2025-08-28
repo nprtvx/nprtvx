9c7f9e5a79e0ea85e78015573874fb2ff91e10cd
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-08-28 21:14:15 +0530

    Update home.py

--- src/home.py
+++ src/home.py
@@ -1,12 +1,12 @@
-home = f" /
-{{% extends "base.html" %}} /
-{{% block header %}} /
-<link rel='stylesheet href='/static/home_styles.css'> /
-{{% endblock %}} /
-{{% block content %}} /
-<div class="nprtvx"> /
-<a href='/' id="nxtitle">nprtvx</a> /
-<div id="nx"></div> /
-<script src='/static/ js/nxtitle.js'></script> /
-{{% endblock %}} /
-"
+home = f"""
+{{% extends "base.html" %}}
+{{% block header %}}
+<link rel='stylesheet href='/static/home_styles.css'>
+{{% endblock %}}
+{{% block content %}}
+<div class="nprtvx">
+<a href='/' id="nxtitle">nprtvx</a>
+<div id="nx"></div>
+<script src='/static/ js/nxtitle.js'></script>
+{{% endblock %}}
+"""
