f9b645f12a77e7aba6c74efbe4116c672d413a4f
Popeye <63921078+nprtvx@users.noreply.github.com>
2025-08-28 22:01:11 +0530

    Update home.py

--- src/home.py
+++ src/home.py
@@ -1,7 +1,9 @@
 home = f"""
+\{% extends "base.html" %\} \{% block header %\} \{% endblock %\} \{% block content %\}
 <div class="nprtvx">
 <a href='/' id="nxtitle">nprtvx</a>
 <div id="nx"></div>
 <script src='/static/js/nxtitle.js'></script>
-</div>
+</div>  
+\{% endblock %\}
 """
